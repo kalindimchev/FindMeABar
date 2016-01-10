e02f34f9fabc65ab08bb5bcbaaa5d701dadf4cb4
kalindimchev <kalin.dimchev@gmail.com>
2016-01-11 00:50:23 +0200

    Menu Fragments java and xml added Some hardcored data for designing UI

--- FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/MainActivity.java
+++ FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.findmeabar.findmeabar;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_menu);
+
+        viewPager = (ViewPager) findViewById(R.id.vp_menu);
+        CustomViewPagerAdapter adapter = new CustomViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+    }
+
+    public static class CustomViewPagerAdapter extends FragmentPagerAdapter {
+
+        public CustomViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new SaladsFragment();
+                case 1:
+                    return new FoodsFragment();
+                case 2:
+                    return new DrinksFragment();
+                default:
+                    return null;
+            }
+        }
+
+
     }
 }
