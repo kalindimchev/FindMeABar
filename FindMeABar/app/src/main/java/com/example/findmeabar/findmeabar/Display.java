21b45e1ada8536409fe07370b60b7e6fc798737b
kasav <ka.valkov81@gmail.com>
2016-01-12 23:37:36 +0200

    Login classes. Display class and layout are for testing only

---
+++ FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/Display.java
@@ -0,0 +1,14 @@
+package com.example.findmeabar.findmeabar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//just for testing
+public class Display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_display);
+    }
+}
--- FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/MainActivity.java
+++ FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.findmeabar.findmeabar;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -46,7 +47,17 @@ public class MainActivity extends AppCompatActivity {
                     return null;
             }
         }
+    }
 
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.b_login_btn){
+            Intent i = new Intent(this, Display.class);
+            startActivity(i);
+        }
 
+        if(v.getId() == R.id.b_signin_btn){
+            Intent i = new Intent(this, Signup.class);
+            startActivity(i);
+        }
     }
 }
