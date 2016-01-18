0e4d77acd74ca26f29fd98768cd6a77b7bc50488
kalindimchev <kalin.dimchev@gmail.com>
2016-01-18 14:12:59 +0200

    User added

---
+++ FindMeABar/app/src/main/java/com/example/findmeabar/findmeabar/User.java
@@ -0,0 +1,36 @@
+package com.example.findmeabar.findmeabar;
+
+import com.telerik.everlive.sdk.core.model.base.DataItem;
+import com.telerik.everlive.sdk.core.serialization.ServerProperty;
+import com.telerik.everlive.sdk.core.serialization.ServerType;
+
+
+@ServerType("User")
+public class User extends DataItem {
+
+    @ServerProperty("Name")
+    private String userName;
+
+    @ServerProperty("Pass")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+
+}
